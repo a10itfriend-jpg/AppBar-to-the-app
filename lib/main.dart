7d30762f5c5f2c301c00b52705a09c78b8064b7c
Ivan Obasuyi <a10it.friend@gmail.com>
2026-02-03 21:57:43 -0500

    New changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {    
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           elevation: 3,
           title: Text('New App Bar '),
@@ -27,21 +28,111 @@ class MainApp extends StatelessWidget {
             ),
           IconButton(
             onPressed:  () {},
-            icon: Icon(Icons.upload, color: Colors.blue),
+            icon: Icon(Icons.ios_share_outlined, color: Colors.white),
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.only(top: 25.0),
-        margin: EdgeInsets.symmetric(horizontal: 20.0),
-        width: double.infinity,
-        height: 150,
-        color: Colors.green,
-        child: Container(
-          width: 300,
-          height: 100,
-          color: Colors.red,)
-        ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            width: double.infinity,
+            height: 140,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 40, 40, 40),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sleep",
+                         style: TextStyle(color: Colors.white, fontSize: 30)
+                      ),
+                      Icon(Icons.chevron_right, color: Colors.white, size: 30),
+                    ],
+                  ),
+                ),
+                Text("By Allison S", style: TextStyle(color: Colors.grey)),
+                Row(children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                      width: 80,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 50, 50, 50),
+                        borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Very relaxing", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 12)
+                    ),
+                  ),
+                                    Container(
+                      width: 80,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 50, 50, 50),
+                        borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Pleasant", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 12)
+                    ),
+                  ),
+                                    Container(
+                      width: 60,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 50, 50, 50),
+                        borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text("Deep", 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 12)
+                    ),
+                  ),
+
+
+                ],)
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            width: double.infinity,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 40, 40, 40),
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            width: double.infinity,
+            height: 130,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 40, 40, 40),
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            width: double.infinity,
+            height: 130,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 40, 40, 40),
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ],
+      ),
       ),
     );
   }
